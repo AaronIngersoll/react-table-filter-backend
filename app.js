bbfc9b39b69d68c3b047d8c5d58c11570db51e9d
Aaron Ingersoll <flex.box.hero@gmail.com>
2023-01-12 11:53:41 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 if (require.main === module) {
-	throw new Error('Do not run directly. Use server.js to start.');
+	throw new Error("Do not run directly. Use server.js to start.");
 }
 
-const Koa = require('koa');
-const koaStatic = require('koa-static');
-
+const Koa = require("koa");
+var cors = require("koa-cors");
+const koaStatic = require("koa-static");
+var options = {
+	origin: "*",
+};
 const app = new Koa();
 
-
 // Register Routes
-let indexRouter = require('./routes/index');
+app.use(cors(options));
+let indexRouter = require("./routes/index");
 
-app.use(indexRouter.routes())
-  .use(indexRouter.allowedMethods());
-
+app.use(indexRouter.routes()).use(indexRouter.allowedMethods());
 
 // Serve static files from public directory
-app.use(koaStatic('./public'));
-
+app.use(koaStatic("./public"));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,35 @@
-const KoaRouter = require('koa-router');
-
-
+const KoaRouter = require("koa-router");
 const indexRouter = new KoaRouter();
+var db = require("../data/database.js");
 
-indexRouter.get('/', async function(ctx) {
-	ctx.body = 'Hello World';
+indexRouter.get("/", async function (ctx) {
+	ctx.body = "Hello World";
 });
-
+indexRouter.get("/members", async function members(ctx, next) {
+	let sql = `SELECT 
+		m.firstName || ' ' || m.lastName AS fullName,
+		m.company,
+    	m.title,
+    	m.department,
+    	m.phone,
+    	m.url,
+    	m.image,
+		a.address ||','|| a.city ||','|| a.state ||' '|| a.zip  AS fullAddress
+		FROM members m 
+		JOIN addresses a on m.id = a.memberId
+		WHERE a.primaryAddress != 0
+		`;
+	return new Promise(function (resolve, reject) {
+		db.all(sql, [], (err, rows) => {
+			if (err) reject(err);
+			else {
+				ctx.status = 200;
+				ctx.type = "application/json";
+				ctx.body = { result: JSON.stringify(rows) };
+				resolve({ rows: rows });
+			}
+		});
+	});
+});
 
 module.exports = indexRouter;
